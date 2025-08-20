f1d6803b95524ebdb0a1e46141ccb86fc2fdb4e8
fuyjcnbr <tmp_mail@tmp_mail.tmp_mail>
2025-08-20 20:32:19 +0300

    files added

--- readme.md
+++ readme.md
@@ -3,7 +3,7 @@ docker build --no-cache --squash -t python_hw_3 -f Dockerfile .
 
 docker run -it python_hw_3 /bin/bash
 
-docker run -v /home/oem/PycharmProjects/python_2025_3:/src -it python_hw_3 /bin/bash
+docker run -v ~/PycharmProjects/python_2025_3:/src -it python_hw_3 /bin/bash
 
 make typing
 
---
+++ src/advanced/callable-protocol.py
@@ -0,0 +1,15 @@
+"""
+TODO:
+
+Define a callable type that accepts a string parameter called `name` and returns None.
+"""
+
+# from typing import Callable, TypedDict, Unpack, Generic
+#
+# class P(TypedDict):
+# 	name: str
+#
+# T = Callable[Unpack[P], None]
+#
+# class SingleStringInput(T):
+# 	...
